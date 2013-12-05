2a513a55d679de9da60368d8234bd1ee61ea4464
Nicolas <nthomas@robotikdose.com>
2013-12-05 15:43:31 +0100

    05121525

--- www/js/services.js
+++ www/js/services.js
@@ -8,9 +8,9 @@ angular.module('starter.services', [])
 
   // Some fake testing data
   var pets = [
-    { id: 0, title: 'Cats', description: 'Furry little creatures. Obsessed with plotting assassination, but never following through on it.' },
-    { id: 1, title: 'Dogs', description: 'Lovable. Loyal almost to a fault. Smarter than they let on.' },
-    { id: 2, title: 'Turtles', description: 'Everyone likes turtles.' },
+    { id: 0, title: 'Nicolas', description: 'Furry little creatures. Obsessed with plotting assassination, but never following through on it.' },
+    { id: 1, title: 'Cliff', description: 'Lovable. Loyal almost to a fault. Smarter than they let on.' },
+    { id: 2, title: 'Dave', description: 'Everyone likes turtles.' },
     { id: 3, title: 'Sharks', description: 'An advanced pet. Needs millions of gallons of salt water. Will happily eat you.' }
   ];
 
